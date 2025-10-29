dca9961dacfdf5f6b3b11577f104d884428099e0
João Guedes <joao.guedes.developer@gmail.com>
2025-10-29 00:45:59 -0300

    ajustes para build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+// next.config.js
+const isCI = process.env.GITHUB_ACTIONS === 'true'
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1]
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+module.exports = {
+  output: 'export',
+  images: { unoptimized: true },
+  // Se for GitHub Pages de projeto (https://user.github.io/<repo>), ative basePath/assetPrefix:
+  ...(isCI && repo
+    ? { basePath: `/${repo}`, assetPrefix: `/${repo}/` }
+    : {}),
+  // trailingSlash opcional para evitar redirects no Pages:
+  // trailingSlash: true,
+}
